04e5832319489e910979199aee7ba1b4b1b2fbd9
Mykola Yarovenko <yarovenko.mykola@gmail.com>
2023-04-23 23:58:59 +0300

    base webpack build config

---
+++ config/build/buildPlugins.ts
@@ -0,0 +1,19 @@
+import HtmlWebpackPlugin    from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import {
+    WebpackPluginInstance,
+    ProgressPlugin }        from 'webpack';
+import { BuildPaths }       from "./types/config";
+
+export default function (paths: BuildPaths): WebpackPluginInstance[] {
+    return [
+        new HtmlWebpackPlugin({
+            template: paths.html,
+        }),
+        new ProgressPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css'
+        })
+    ];
+}
---
+++ config/build/index.ts
@@ -0,0 +1,11 @@
+import plugins            from './buildPlugins';
+import loaders            from './buildLoaders';
+import resolvers          from './buildResolvers';
+import devServer          from './buildDevServer';
+
+export default {
+    resolvers,
+    devServer,
+    plugins,
+    loaders,
+};
