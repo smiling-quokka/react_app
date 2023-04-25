1e7fe792371582e292a152890895a26fdeaae34a
Mykola Yarovenko <mykola.yarovenko@masterofcode.com>
2023-04-25 23:10:57 +0300

    theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { Route, Routes, Link } from 'react-router-dom';
 
 import './styles/index.scss';
 
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
-
-export enum Theme {
-    LIGHT = 'light',
-    DARK = 'dark',
-};
+import { useTheme } from './styles/theme/useTheme';
 
 export const App = () => {
-    const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
-
-    const toggleTheme = () => {
-        setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
-    };
+    const { theme, toggleTheme } = useTheme();
 
     return (
         <div className={`app ${theme}`}>
-            <button onClick={toggleTheme}>change theme</button>
+            <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>home</Link>
             <Link to={'/about'}>about</Link>
             <Suspense fallback={<div>Loading...</div>}>
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { BrowserRouter } from 'react-router-dom';
+import ThemeProvider from './styles/theme/ThemeProvider';
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </BrowserRouter>,
 );
